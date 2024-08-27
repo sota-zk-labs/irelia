b31add8c42c07b0e1456840cc77855959e518d95
draply <doduc0610@gmail.com>
2024-08-27 11:38:12 +0000

    feat: optimize register_continuous_page_batch

--- src/contracts_caller/memory_page_fact_registry/mod.rs
+++ src/contracts_caller/memory_page_fact_registry/mod.rs
@@ -1,3 +1,4 @@
+mod register_continuous_page;
 pub mod register_continuous_page_batch;
 pub mod register_memory;
 pub mod types;
--- tests/memory_page_test.rs
+++ tests/memory_page_test.rs
@@ -7,7 +7,9 @@ mod tests {
     use aptos_testcontainer::test_utils::aptos_container_test_utils::{lazy_aptos_container, run};
 
     use verifier_onchain_services::config::{AppConfig, EnvConfig};
-    use verifier_onchain_services::contracts_caller::memory_page_fact_registry::register_memory::register_memory;
+    use verifier_onchain_services::contracts_caller::memory_page_fact_registry::register_memory::{
+        large_register_memory_batch, register_memory, register_memory_batch,
+    };
 
     #[tokio::test]
     pub async fn memory_page_test() {
@@ -57,8 +59,9 @@ mod tests {
                     .await
                     .unwrap();
 
-                register_memory(&config).await.unwrap();
-
+                //register_memory_batch(&config).await.unwrap();
+                //register_memory(&config).await.unwrap();
+                large_register_memory_batch(&config).await.unwrap();
                 Ok(())
             })
         })
