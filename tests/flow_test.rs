f1bd252648df2b10876e231fbd0cb576904d3534
Steve Nguyen <sonntuet1997@gmail.com>
2024-08-28 15:13:06 +0000

    feat: test env override

--- tests/flow_test.rs
+++ tests/flow_test.rs
@@ -2,10 +2,10 @@
 mod tests {
     use std::collections::HashMap;
 
-    use aptos_sdk::types::chain_id::NamedChain::TESTING;
     use aptos_sdk::types::LocalAccount;
     use aptos_testcontainer::test_utils::aptos_container_test_utils::{lazy_aptos_container, run};
     use log::info;
+    use test_log::test;
 
     use verifier_onchain_services::config::{AppConfig, EnvConfig};
     use verifier_onchain_services::contracts_caller::verify_fri::sample_verify_fri_input::sample_verify_fri_input;
@@ -13,26 +13,24 @@ mod tests {
     use verifier_onchain_services::contracts_caller::verify_merkle::sample_verify_merkle_input::sample_verify_merkle_input;
     use verifier_onchain_services::contracts_caller::verify_merkle::verify_merkle::verify_merkle;
 
-    #[tokio::test]
+    #[test(tokio::test)]
     pub async fn verifier_test() {
         run(2, |accounts| {
             Box::pin(async move {
                 let aptos_container = lazy_aptos_container().await.unwrap();
-                let node_url = aptos_container.get_node_url().await.unwrap();
-
+                let node_url = aptos_container.get_node_url();
                 let module_account_private_key = accounts.first().unwrap();
                 let module_account =
                     LocalAccount::from_private_key(module_account_private_key, 0).unwrap();
 
                 let sender_account_private_key = accounts.get(1).unwrap();
-                let sender_account =
-                    LocalAccount::from_private_key(sender_account_private_key, 0).unwrap();
 
+                let module_address = module_account.address().to_string();
                 let config = AppConfig::from(EnvConfig {
                     node_url,
                     private_key: sender_account_private_key.to_string(),
-                    module_address: module_account.address().to_string(),
-                    chain_id: TESTING.id().to_string(),
+                    module_address: module_address.clone(),
+                    chain_id: aptos_container.get_chain_id().to_string(),
                 });
 
                 let sequence_number = config
@@ -44,12 +42,8 @@ mod tests {
                 config.account.set_sequence_number(sequence_number);
 
                 let mut named_addresses = HashMap::new();
-                named_addresses.insert(
-                    "verifier_addr".to_string(),
-                    module_account.address().to_string(),
-                );
-                named_addresses
-                    .insert("lib_addr".to_string(), module_account.address().to_string());
+                named_addresses.insert("verifier_addr".to_string(), module_address.clone());
+                named_addresses.insert("lib_addr".to_string(), module_address);
                 aptos_container
                     .upload_contract(
                         "./contracts/navori",
@@ -71,7 +65,8 @@ mod tests {
                         height,
                         expected_root,
                     )
-                    .await?;
+                    .await
+                    .unwrap();
                     info!("Verify Merkle {} success", i);
                 }
 
@@ -93,7 +88,8 @@ mod tests {
                         fri_step_size,
                         expected_root,
                     )
-                    .await?;
+                    .await
+                    .unwrap();
                     info!("Verify FRI {} success", i);
                 }
                 Ok(())
